7ef193aed97c52c8657ad7cfa6c7637452c4d80e
tao nguyen <tao nguyen>
2024-08-23 10:04:20 +0700

    header UI #1

--- src/component/Layout/DefautLayout/Header/index.js
+++ src/component/Layout/DefautLayout/Header/index.js
@@ -1,10 +1,29 @@
 import style from './Header.module.scss';
 import classNames from 'classnames/bind';
+import image from '../../../../assets/image';
+import { FaCircleXmark, FaMagnifyingGlass } from 'react-icons/fa6';
+import { BiLoaderCircle } from 'react-icons/bi';
 function Header() {
     const cx = classNames.bind(style);
     return (
         <header className={cx('wrapper')}>
-            <div className={cx('inner')}></div>
+            <div className={cx('inner')}>
+                <img src={image.logo.default} alt="tiktok" />
+
+                <div className={cx('search')}>
+                    <input placeholder="Search" spellCheck={false} className={cx('search-input')} />
+
+                    <button className={cx('search-clear')}>
+                        <FaCircleXmark />
+                    </button>
+                    <BiLoaderCircle className={cx('search-loading')} />
+                    <button className={cx('search-btn')}>
+                        <FaMagnifyingGlass />
+                    </button>
+                </div>
+
+                <div className={cx('action')}></div>
+            </div>
         </header>
     );
 }
